b33701225b3ed200d49271f8546f79bfaaf96288
hamenok <zdrapalo@gmail.com>
2021-11-12 14:16:44 +0300

    Исправил название вендора и модуля

---
+++ AmastyHamenok/ModuleHamenok/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AmastyHamenok_ModuleHamenok', __DIR__);
--- Amasty_Hamenok/module_hamenok/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Amasty_Hamenok_module-hamenok', __DIR__);
